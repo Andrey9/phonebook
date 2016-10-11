e265c44066b46b4022cc84eeda65840eeaeb60bf
DESKTOP-6J247ML\Holden <yavor@ua.fm>
2016-10-11 21:36:24 +0300

    пробую ше раз

--- resources/views/hab/add.blade.php
+++ resources/views/hab/add.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Holden
- * Date: 11.10.2016
- * Time: 21:04
- */
+@extends('hab.main')
+@section('content')
+    <div class="container">
+    <form action="{{url ('/main')}}" class="col-lg-4 col-lg-offset-4">
+
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="name">
+
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="surname">
+
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="nomer">
+
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="year">
+
+        </div>
+
+        <div class="form-group">
+            <input type="submit" class="form-control btn btn-success" value="add">
+
+        </div>
+    </form>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @stop
--- resources/views/hab/table.blade.php
+++ resources/views/hab/table.blade.php
@@ -1,7 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Holden
- * Date: 11.10.2016
- * Time: 21:06
- */
+@extends('hab.main')
+@section('content')
+<div class="container">
+    <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading">Panel heading</div>
+
+        <!-- Table -->
+        <table class="table">
+            <tr>
+                <td>name</td>
+                <td>surname</td>
+                <td>nomer</td>
+                <td>year</td>
+                <td>
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-remove"></span></a>
+
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-edit"></span></a>
+                </td>
+            </tr>
+            <tr>
+                <td>name</td>
+                <td>surname</td>
+                <td>nomer</td>
+                <td>year</td>
+                <td>
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-remove"></span></a>
+
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-edit"></span></a>
+                </td>
+            </tr>
+            <tr>
+                <td>name</td>
+                <td>surname</td>
+                <td>nomer</td>
+                <td>year</td>
+                <td>
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-remove"></span></a>
+
+                    <a href="" class="btn btn-default pull-right">
+                        <span class="glyphicon glyphicon-edit"></span></a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+    @stop
