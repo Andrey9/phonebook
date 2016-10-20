f5c172307e3f3e79c8558c831f271c5afa5ddc0f
DESKTOP-6J247ML\Holden <yavor@ua.fm>
2016-10-20 21:06:03 +0300

    Попитка номер два, нові оновлення!!!

--- resources/views/hab/form.blade.php
+++ resources/views/hab/form.blade.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Holden
- * Date: 20.10.2016
- * Time: 20:33
- */
+<div class="form-group">
+    {!! Form::text('name',null,['class'=>'form-control']) !!}
+
+</div>
+
+<div class="form-group">
+    {!! Form::text('surname',null,['class'=>'form-control']) !!}
+
+</div>
+
+<div class="form-group">
+    {!! Form::text('nomer',null,['class'=>'form-control']) !!}
+
+</div>
+
+<div class="form-group">
+    {!! Form::text('date','year',null,['class'=>'form-control']) !!}
+
+</div>
+
+<div class="form-group">
+    {!! Form::submit('save',['class'=>'form-control btn btn-success']) !!}
+
+</div>
