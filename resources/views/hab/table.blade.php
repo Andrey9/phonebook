7e86d15b8edf17a8eda5ee8ec3cec63f2b8f4ab1
DESKTOP-6J247ML\Holden <yavor@ua.fm>
2016-10-21 12:07:03 +0300

    Вроді остані зміни!!!

--- resources/views/hab/table.blade.php
+++ resources/views/hab/table.blade.php
@@ -7,17 +7,21 @@
 
         <!-- Table -->
         <table class="table">
-            <th>
+            <thead>
+            <tr>
                 <td>Ім'я</td>
                 <td>Прізвище</td>
                 <td>Ромер</td>
                 <td>Дата народження</td>
-                <td>
-                    Керування
-                </td>
-            </th>
+
+            </tr>
+
+
+            </thead>
             @if(!$records)
-                <td colspan="5">Немає записів</td>
+                <tr>
+                <td colspan="4">Немає записів</td>
+                </tr>
             @else
 
             @foreach($records as $record)
@@ -26,10 +30,10 @@
                 <td>{{$record->name}}</td>
                 <td>{{$record->surname}}</td>
                 <td>{{$record->number}}</td>
-                <td>{{$record->year}}</td>
+                <td>{{$record->year->format('d.m.Y')}}</td>
                 <td>
-                    {!! Form::open(['method' => 'DELETE', 'url' => '/record/'.$record->id]) !!}
-                    {!! Form::button('<span class="glyphicon glyphicon-remove"></span>',['class' => 'button','type'=>'submit']) !!}
+                    {!! Form::open(['method' => 'DELETE', 'url' => '/record/'.$record->id,'class'=>'pull-right']) !!}
+                    {!! Form::button('<span class="glyphicon glyphicon-remove"></span>',['class' => 'btn btn-default','type'=>'submit']) !!}
                     {!! Form::close() !!}
 
                     <a href="{{url('/record/'.$record->id.'/edit')}}" class="btn btn-default pull-right">
